2f01acd1530893a81ad0bd924b58cf6717562618
luvlaceeeee <nikitan2002@gmail.com>
2023-07-23 16:00:07 +0300

    fix: URL

--- config/URL.ts
+++ config/URL.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "xn--d1almjy9c.xn--p1ai/api/v1"
-export const AUTH_URL = "xn--d1almjy9c.xn--p1ai/auth"
+export const BASE_API_URL = "http://xn--d1almjy9c.xn--p1ai/api/v1"
+export const AUTH_URL = "http://xn--d1almjy9c.xn--p1ai/auth"
